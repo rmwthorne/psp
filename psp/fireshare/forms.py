19a052de185977bfcd3c04e41e721b6df9410283
Ross Thorne <ross@aigenpulse.com>
2019-03-07 23:49:00 +0000

    Define views and form

---
+++ psp/fireshare/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from .models import FileUpload
+
+
+class FileUploadForm(forms.ModelForm):
+    class Meta:
+        model = FileUpload
+        fields = ('name', 'upload', 'published')
+
--- psp/fireshare/views.py
+++ psp/fireshare/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
 
-# Create your views here.
-class FileList(View):
-    def get(self, request):
-        pass
+from .models import FileUpload
+from .forms import FileUploadForm
 
-    def post(self, request):
-        pass
+
+class FileListView(ListView):
+    model = FileUpload
+    context_object_name = 'uploads'
+
+
+class UploadFileView(CreateView):
+    model = FileUpload
+    form_class = FileUploadForm
+    success_url = reverse_lazy('files_list')
