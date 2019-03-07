45dc23a4543b02ca510065b98f01e91c8886602b
Ross Thorne <ross@aigenpulse.com>
2019-03-07 23:03:21 +0000

    Fix app reference

--- psp/settings.py
+++ psp/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'fireshare.apps.FireshareConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'psp.fireshare',
 ]
 
 MIDDLEWARE = [
